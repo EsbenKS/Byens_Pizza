bf04c73a6794065a4483ff1964651992581ece63
EsbenKS <33330790+EsbenKS@users.noreply.github.com>
2019-04-03 14:45:58 +0200

    Add files via upload

---
+++ Models/Pizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace PizzaBaren.Models
+{
+    public class Pizza { 
+        public Guid Id { get; set; }
+        public int PizzaNumber { get; set; }
+        public string PizzaName { get; set; }
+        public string Ingredients { get; set; }
+        public int Pris { get; set; }
+        
+    }
+}
